9a51a9358c4772924d1ff93c402db654b079c150
Andreas Ma <andreasma1@outlook.de>
2026-03-02 22:52:00 +0100

    added login test, use registerrequest dto
    feat: use RegisterRequest DTO instead of passing user when testing
    Register
    test: AuthController - Login

--- Controllers/AuthControllerTests.cs
+++ Controllers/AuthControllerTests.cs
@@ -3,6 +3,8 @@ using Microsoft.EntityFrameworkCore;
 using Timebox.Controllers;
 using TimeboxTask = Timebox.Models.Task;
 using TimeboxUser = Timebox.Models.User;
+using LoginRequest = Timebox.Models.LoginRequest;
+using RegisterRequest = Timebox.Models.RegisterRequest;
 using BCrypt.Net;
 
 [TestFixture]
@@ -33,19 +35,43 @@ public class AuthControllerTests
     [Test]
     public async Task AuthController_Register_ReturnsCreatedUser()
     {
-        var newUser = new TimeboxUser
+
+        var registerRequest = new RegisterRequest
+        {
+            Email = "test@testdomain.com",
+            Username = "testuser",
+            Password = "password123"
+        };
+
+        var result = await _controller.Register(registerRequest);
+        var newUser = _context.Users.FirstOrDefault(u => u.Email == registerRequest.Email);
+
+        Assert.That(newUser != null);
+        Assert.That(_context.Users.Count() == 1);
+    }
+    [Test]
+    public async Task AuthController_Login_ReturnJWTToken()
+    {
+
+        var testUser = new TimeboxUser
         {
             Username = "testuser",
             Email = "test@testdomain.com",
             PasswordHash = BCrypt.Net.BCrypt.HashPassword("password123")
+        };
+        _context.Users.Add(testUser);
+        await _context.SaveChangesAsync();
 
+        var loginRequest = new LoginRequest
+        {
+            Email = "test@testdomain.com",
+            Password = "password123"
         };
 
-        var result = await _controller.Register(newUser);
-        var createdUser = result.Value;
+        var result = await _controller.Login(loginRequest);
+        var token = result.Value;
+        Assert.That(token != null);
+        Assert.That(token.Length > 0);
 
-        Assert.That(createdUser != null);
-        Assert.That(_context.Users.Count() == 1);
-        Assert.That(createdUser.Username == newUser.Username);
     }
 }
